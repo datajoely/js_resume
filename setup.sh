bf68ca93e2a6e8dceb4445e554f25f060a7f731a
datajoely <joel.schwarzmann@quantumblack.com>
2023-07-25 11:39:11 +0100

    delete test script
    Signed-off-by: datajoely <joel.schwarzmann@quantumblack.com>

--- setup.sh
+++
@@ -1,3 +0,0 @@
-npm install resumed jsonresume-theme-macchiato
-npm install -g puppeteer-cli
-puppeteer --wait-until networkidle0 --margin-top 0 --margin-right 0 --margin-bottom 0 --margin-left 0 --format A4 print resume.html resume.pdf
